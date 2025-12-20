cac0a6b30192d7760b268d3cce1098fbcf47cc2c
Saw Simeon <doe_doe207@hotmail.com>
2025-12-20 19:18:22 +0100

    added mod.rs file for stablecoin

--- src/stablecoin/mod.rs
+++ src/stablecoin/mod.rs
@@ -15,15 +15,65 @@ pub mod get_realtime_exchange_rate;
 
 pub fn router() -> Router<AppState> {
     Router::new()
-        .route("/", axum::routing::get(get_available_stablecoins::get_available_stablecoins))
-        .route("/supply-caps", axum::routing::get(get_supply_caps::get_supply_caps))
-        .route("/quote", axum::routing::post(get_mint_redeem_quote::get_mint_redeem_quote))
-        .route("/mint/tx", axum::routing::post(generate_mint_transaction::generate_mint_transaction))
-        .route("/burn/tx", axum::routing::post(generate_burn_transaction::generate_burn_transaction))
-        .route("/apy", axum::routing::get(get_all_apy::get_all_apy))
-        .route("/exchange-rates", axum::routing::get(get_latest_exchange_rates::get_latest_exchange_rates))
-        .route("/:stablecoin/historical-rates", axum::routing::get(get_historical_exchange_rates::get_historical_exchange_rates))
-        .route("/:stablecoin/apy", axum::routing::get(get_specific_apy::get_specific_apy))
-        .route("/:stablecoin/historical-apy", axum::routing::get(get_historical_apy::get_historical_apy))
-        .route("/:stablecoin/realtime-rate", axum::routing::get(get_realtime_exchange_rate::get_realtime_exchange_rate))
+        // Stablecoin metadata
+        .route(
+            "/",
+            axum::routing::get(get_available_stablecoins::get_available_stablecoins),
+        )
+        .route(
+            "/supply-caps",
+            axum::routing::get(get_supply_caps::get_supply_caps),
+        )
+
+        // Quotes
+        .route(
+            "/quote",
+            axum::routing::post(get_mint_redeem_quote::get_mint_redeem_quote),
+        )
+
+        // Mint / Burn transactions
+        .route(
+            "/mint/tx",
+            axum::routing::post(generate_mint_transaction::generate_mint_transaction),
+        )
+        .route(
+            "/burn/tx",
+            axum::routing::post(generate_burn_transaction::generate_burn_transaction),
+        )
+
+        // APY (all stablecoins)
+        .route(
+            "/apy",
+            axum::routing::get(get_all_apy::get_all_apy),
+        )
+
+        // Latest exchange rates (all stablecoins)
+        .route(
+            "/exchange-rates",
+            axum::routing::get(get_latest_exchange_rates::get_latest_exchange_rates),
+        )
+
+        // Historical exchange rates for a specific stablecoin
+        .route(
+            "/stablecoin/:index/exchange-rates/historical",
+            axum::routing::get(get_historical_exchange_rates::get_historical_exchange_rates),
+        )
+
+        // Specific APY for a stablecoin
+        .route(
+            "/stablecoin/:index/apy",
+            axum::routing::get(get_specific_apy::get_specific_apy),
+        )
+
+        // Historical APY for a stablecoin
+        .route(
+            "/stablecoin/:index/apy/historical",
+            axum::routing::get(get_historical_apy::get_historical_apy),
+        )
+
+        // Realtime exchange rate for a stablecoin
+        .route(
+            "/stablecoin/:index/exchange-rate",
+            axum::routing::get(get_realtime_exchange_rate::get_realtime_exchange_rate),
+        )
 }
